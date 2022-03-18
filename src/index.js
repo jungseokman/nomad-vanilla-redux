253fbf6fe74ee8fd6577d019886289c8aa9ed57e
jungseokman <seokman.jung@gmail.com>
2022-03-18 16:32:56 +0900

    redux-first

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
+import { createStore } from "redux";
 
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.getElementById("number");
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+};
+// reducer는 함수이며 data를 modify한다.
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+add.addEventListener("click", () => countStore.dispatch({ type: ADD }));
+minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
